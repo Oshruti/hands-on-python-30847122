fc2ccd9f5c2826385b9e8f6231069d8ca561e9b9
Oshruti <123701518+Oshruti@users.noreply.github.com>
2023-12-22 16:53:50 +0000

    Day1-Lessons

--- Ex_Files/02_02_begin/main.py
+++ Ex_Files/02_02_begin/main.py
@@ -7,6 +7,9 @@ intrupution = f"Hello {name}"
 
 greet_format = "Hello {}"
 
-formatted = greet_format.format(name)
+formatted = greet_format.format(name) #Helps to change what we put in name
 
 print(intrupution, formatted)
+print(formatted.upper())
+print(formatted.lower())
+print(formatted.replace("John", "Paul"))
